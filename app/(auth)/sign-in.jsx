7851087d9996980269dfdf87e2af2941541a067c
Satya <satyajitborade99@gmail.com>
2025-02-12 00:01:38 +0530

    add background image in sign-in

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,11 +1,15 @@
-import { View, Text } from 'react-native'
+import { View, Text, ImageBackground, TouchableOpacity } from "react-native";
 import React from 'react'
+import { router } from "expo-router";
 
 const signin = () => {
   return (
-    <View>
-      <Text></Text>
-    </View>
+      <ImageBackground
+          source={require("../../assets/images/authbg.png")}
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+          resizeMode="cover"
+       ></ImageBackground>
+   
   )
 }
 
